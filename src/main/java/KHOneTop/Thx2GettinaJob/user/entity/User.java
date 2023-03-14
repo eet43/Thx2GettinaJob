6ea1e8a76e7b060faeae796294f2b855470369fa
daehee <1998opening@gmail.com>
2023-03-14 15:39:24 +0900

    feat : User Entity 생성

---
+++ src/main/java/KHOneTop/Thx2GettinaJob/user/entity/User.java
@@ -0,0 +1,32 @@
+package KHOneTop.Thx2GettinaJob.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "email", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "nickname", nullable = false, unique = true)
+    private String nickname;
+
+}
