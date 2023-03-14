786a7b9d5a0715b933fb942ead4edeb52b39a3eb
daehee <1998opening@gmail.com>
2023-03-14 15:14:17 +0900

    init

---
+++ src/main/java/KHOneTop/Thx2GettinaJob/Thx2GettinaJobApplication.java
@@ -0,0 +1,13 @@
+package KHOneTop.Thx2GettinaJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thx2GettinaJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thx2GettinaJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KHOneTop/Thx2GettinaJob/Thx2GettinaJobApplicationTests.java
@@ -0,0 +1,13 @@
+package KHOneTop.Thx2GettinaJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thx2GettinaJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
