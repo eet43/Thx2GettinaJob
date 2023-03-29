b5601e33aad32339aa96ebbdd2c9a374bd1d07af
daehee <1998opening@gmail.com>
2023-03-29 17:26:56 +0900

    feat : Exam 예제 코드 작성

---
+++ src/main/java/KHOneTop/Thx2GettinaJob/exam/entity/ExamTimeStamp.java
@@ -0,0 +1,28 @@
+package KHOneTop.Thx2GettinaJob.exam.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Embeddable
+public class ExamTimeStamp {
+
+    @Column(name = "examDate", nullable = false)
+    private LocalDateTime examDate;
+
+    @Column(name = "regStartDate", nullable = false)
+    private LocalDateTime regStartDate;
+
+    @Column(name = "regEndDate", nullable = false)
+    private LocalDateTime regEndDate;
+
+    @Column(name = "addRegStartDate", nullable = false)
+    private LocalDateTime addRegStartDate;
+
+    @Column(name = "addRegEndDate", nullable = false)
+    private LocalDateTime addRegEndDate;
+}
