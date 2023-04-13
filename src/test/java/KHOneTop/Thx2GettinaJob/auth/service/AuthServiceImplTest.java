c46e5b2bb9da084f82c7741277d31b2cae7dff67
daehee <1998opening@gmail.com>
2023-04-13 16:27:50 +0900

    feat : 로그인 로직 테스트 완료

--- src/test/java/KHOneTop/Thx2GettinaJob/auth/service/AuthServiceImplTest.java
+++ src/test/java/KHOneTop/Thx2GettinaJob/auth/service/AuthServiceImplTest.java
@@ -1,6 +1,10 @@
 package KHOneTop.Thx2GettinaJob.auth.service;
 
+import KHOneTop.Thx2GettinaJob.auth.JwtProvider;
+import KHOneTop.Thx2GettinaJob.auth.dto.LoginRequest;
+import KHOneTop.Thx2GettinaJob.auth.dto.LoginToken;
 import KHOneTop.Thx2GettinaJob.auth.dto.SignUpRequest;
+import KHOneTop.Thx2GettinaJob.common.response.Codeset;
 import KHOneTop.Thx2GettinaJob.common.response.CustomException;
 import KHOneTop.Thx2GettinaJob.user.entity.User;
 import KHOneTop.Thx2GettinaJob.user.repository.UserRepository;
@@ -11,7 +15,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,11 +32,17 @@ class AuthServiceImplTest {
     private AuthServiceImpl authService;
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtProvider jwtProvider;
+
 
     final String email = "test@gmail.com";
     final String validPassword = "1q2w3e4r!";
     final String invalidPassword1 = "1q2e3@";
     final String invalidPassword2 = "1q2e31251";
+    final String encodePassword = "encodePassword";
     final String name = "김달순";
     final String nickname = "김덕자";
 
@@ -42,7 +55,9 @@ class AuthServiceImplTest {
         given(userRepository.findByNickname(nickname)).willReturn(Optional.of(new User()));
 
         //when & then
-        assertThatThrownBy(()-> authService.SignUp(signUpRequest)).isInstanceOf(CustomException.class);
+        assertThatThrownBy(() -> authService.SignUp(signUpRequest))
+                .isInstanceOf(CustomException.class)
+                .hasFieldOrPropertyWithValue("code", Codeset.ALREADY_NICKNAME);
 
     }
 
@@ -50,6 +65,53 @@ class AuthServiceImplTest {
     @DisplayName("비밀번호 검증 테스트")
     void checkPasswordValid() throws Exception {
         assertThat(authService.pwIsValid(invalidPassword2)).isFalse();
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 로그인 검증 테스트")
+    void InvalidLogin() throws Exception {
+        //given
+        LoginRequest loginRequest = new LoginRequest(email, invalidPassword1);
+        User user = User.builder()
+                .email(email)
+                .password(encodePassword)
+                .build();
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(invalidPassword1, encodePassword)).willReturn(false);
+
+        //when & then
+        assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(CustomException.class)
+                .hasFieldOrPropertyWithValue("code", Codeset.INVALID_PASSWORD);
+    }
+
+    @Test
+    @DisplayName("유효한 로그인 검증 테스트")
+    void validLogin() throws Exception {
+        //given
+        LoginRequest loginRequest = new LoginRequest(email, validPassword);
+        User user = User.builder()
+                .email(email)
+                .password(encodePassword)
+                .name(name)
+                .nickname(nickname)
+                .build();
+        Map<String, String> userInfo = new HashMap<>();
+        userInfo.put("email", email);
+        userInfo.put("name", name);
+        userInfo.put("nickname", nickname);
+
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(validPassword, encodePassword)).willReturn(true);
+        given(jwtProvider.generateToken(userInfo)).willReturn("accessToken");
+        given(jwtProvider.generateRefreshToken(userInfo)).willReturn("refreshToken");
+
+        //when
+        LoginToken result = authService.login(loginRequest);
+
+        //then
+        assertThat(result).isInstanceOf(LoginToken.class);
+        assertThat(result.getAccessToken()).isEqualTo("accessToken");
     }
 
 }
